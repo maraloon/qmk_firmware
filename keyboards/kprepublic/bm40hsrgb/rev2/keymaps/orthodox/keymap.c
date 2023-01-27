62408ce3fa2bd17544925004bed5621195b38617
mara <mara@the-witch.ru>
2023-01-28 00:31:57 +0400

    One hand

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/orthodox/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/orthodox/keymap.c
@@ -4,6 +4,9 @@ extern MidiDevice midi_device;
 
 enum layers {
   _ALPHA,
+  _OHLEFT,
+  _OHRIGHT,
+  _OHNUMBER,
   _NOTES,
   _VELO,
   _CHNL,
@@ -20,22 +23,22 @@ enum layers {
 #define _______ KC_NO
 #define ________________ KC_NO
 
-#define Numbers MO(_NUMBER) 
+#define Numbers MO(_NUMBER)
 #define Symbols MO(_SYMBOL)
 #define Velocity MO(_VELO)
 #define Channel MO(_CHNL)
 #define Brackets MO(_BRCKTS)
 #define Mod MO(_MOD)
-#define App MO(_APP) 
-#define ToAlpha TO(_ALPHA) 
-#define ToNotes TO(_NOTES) 
+#define App MO(_APP)
+#define ToAlpha TO(_ALPHA)
+#define ToNotes TO(_NOTES)
 
-#define PRNTSC1 LCAG(KC_1) 
-#define PRNTSC2 LCAG(KC_2) 
-#define PRNTSC3 LCAG(KC_3) 
+#define PRNTSC1 LCAG(KC_1)
+#define PRNTSC2 LCAG(KC_2)
+#define PRNTSC3 LCAG(KC_3)
 
-#define NewLine LSFT(KC_ENT) 
-#define DelWord LALT(KC_BSPC) 
+#define NewLine LSFT(KC_ENT)
+#define DelWord LALT(KC_BSPC)
 
 #define Alfred HYPR(KC_SPC)
 #define Buffer HYPR(KC_V)
@@ -69,16 +72,16 @@ enum layers {
 #define _K KC_K
 #define _H KC_H
 
-#define _0 KC_P0 
-#define _1 KC_P1 
-#define _2 KC_P2 
-#define _3 KC_P3 
-#define _4 KC_P4 
-#define _5 KC_P5 
-#define _6 KC_P6 
-#define _7 KC_P7 
-#define _8 KC_P8 
-#define _9 KC_P9 
+#define _0 KC_P0
+#define _1 KC_P1
+#define _2 KC_P2
+#define _3 KC_P3
+#define _4 KC_P4
+#define _5 KC_P5
+#define _6 KC_P6
+#define _7 KC_P7
+#define _8 KC_P8
+#define _9 KC_P9
 
 #define Up KC_UP
 #define Down KC_DOWN
@@ -87,31 +90,31 @@ enum layers {
 
 #define Space SFT_T(KC_SPC)
 #define Backspace KC_BSPC
-#define Command KC_LCMD 
-#define rcmd KC_RCMD 
+#define Command KC_LCMD
+#define rcmd KC_RCMD
 #define Lang KC_RSFT
-#define Control KC_LCTL 
-#define Alt KC_LALT 
-#define Enter KC_ENT 
-#define Esc KC_ESC 
-#define Tab KC_TAB 
+#define Control KC_LCTL
+#define Alt KC_LALT
+#define Enter KC_ENT
+#define Esc KC_ESC
+#define Tab KC_TAB
 
-#define PgDn KC_PGDN 
-#define PgUp KC_PGUP 
-#define Home KC_HOME 
-#define End KC_END 
+#define PgDn KC_PGDN
+#define PgUp KC_PGUP
+#define Home KC_HOME
+#define End KC_END
 
-#define BackSlash KC_BSLS 
-#define Percent KC_PERC 
-#define Asterisk KC_ASTR 
-#define At KC_AT 
-#define Hash KC_HASH 
-#define Tilda KC_TILD 
-#define Grave KC_GRV 
-#define Equal KC_EQL 
-#define Underscore KC_UNDS 
-#define Minus KC_MINS 
-#define Plus KC_PLUS 
+#define BackSlash KC_BSLS
+#define Percent KC_PERC
+#define Asterisk KC_ASTR
+#define At KC_AT
+#define Hash KC_HASH
+#define Tilda KC_TILD
+#define Grave KC_GRV
+#define Equal KC_EQL
+#define Underscore KC_UNDS
+#define Minus KC_MINS
+#define Plus KC_PLUS
 
 #define Exlm KC_EXLM
 #define Question KC_QUES
@@ -216,12 +219,36 @@ enum layers {
 #define nB1 MI_B_1
 
 
+#define ToOneHand TO(_OHLEFT)
+#define RightHand MO(_OHRIGHT)
+#define ToOHNumbers TO(_OHNUMBER)
+#define OSControl OSM(MOD_LCTL)
+#define OSCommand OSM(MOD_LGUI)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ALPHA] = LAYOUT(
     _,   _Q, _W, _F, _P, _B, _J, _L, _U, _Y, Tmux, _,
     Alt, _A, _R, _S, _T, _G, _M, _N, _E, _I, _O, _,
     _,   _Z, _X, _C, _D, _V, _K, _H, Alt, _, _, _,
-    _, _,  _, Command, Numbers,  Space,  Mod, Control, _, _, _
+    ToOneHand, _,  _, Command, Numbers,  Space,  Mod, Control, _, _, QK_BOOT
+),
+[_OHLEFT] = LAYOUT(
+    ToOHNumbers, _Q, _W, _F, _P, _B, _, _, _, _, _, _,
+    Backspace, _A, _R, _S, _T, _G, _, _, _, _, _, _,
+    _, _Z, _X, _C, _D, _V, _, _, _, _, _, _,
+    ToAlpha, _,  _, _, RightHand,  Space,  _, _, _, _, _
+),
+[_OHRIGHT] = LAYOUT(
+    _, _,  _Y, _U, _L, _J, _, _, _, _, _, _,
+    _, _O, _I, _E, _N,_M, _, _, _, _, _, _,
+    _, _, OSControl, OSCommand, _H, _K, _, _, _, _, _, _,
+    _, _,  _, _, _,  Space,  _, _, _, _, _
+),
+[_OHNUMBER] = LAYOUT(
+    _, _,  _1, _2, _3, _,       _, _, _, _, _, _,
+    ToAlpha, _0, _4, _5, _6, _,       _, _, _, _, _, _,
+    _, _,  _7, _8, _9, _,       _, _, _, _, _, _,
+    _, _,  _,  _,  _,           _, _, _, _, _, _
 ),
 // [_NOTES] = LAYOUT(
 //     nC,  nCs,  nD,  nDs,  nE,  nF,  nFs,  nG,  nGs,  nA,  nAs,  nB,
@@ -247,7 +274,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _, _,  _7, _8, _9, _,       Pipe,      Colon,    Semicolon, Caret, Dollar, _,
     _, _,  _,  _,  _,      DelWord,        Symbols,  _,         _,     _,      _
 ),
-
 [_SYMBOL] = LAYOUT(
     _,_,      _,    KC_LCBR, KC_RCBR, _, _, Asterisk, Percent, BackSlash,  _,_,
     _,KC_LT, KC_GT, KC_LPRN, KC_RPRN, _, _, At,       Hash,    Tilda,      Grave,_,
@@ -296,7 +322,7 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                     case _SYMBOL:
                         rgb_matrix_set_color(index, 0, 0, 20);
                         break;
-                    default: 
+                    default:
                         rgb_matrix_set_color(index, 20, 0, 20);
                         break;
                     }
