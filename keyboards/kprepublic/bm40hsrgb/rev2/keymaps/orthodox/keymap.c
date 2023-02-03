dcace07b7ecd5080b95bafb8fe81f6b791766743
mara <mara@the-witch.ru>
2023-02-03 23:26:14 +0400

    remove midi notes

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/orthodox/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/orthodox/keymap.c
@@ -142,85 +142,6 @@ enum layers {
 #define Bigger LCMD(Plus)
 #define Settings LCMD(Comma)
 
-#define nC MI_C
-#define nCs MI_Cs
-#define nD MI_D
-#define nDs MI_Ds
-#define nE MI_E
-#define nF MI_F
-#define nFs MI_Fs
-#define nG MI_G
-#define nGs MI_Gs
-#define nA MI_A
-#define nAs MI_As
-#define nB MI_B
-
-#define nC1 MI_C_1
-#define nCs1 MI_Cs_1
-#define nD1 MI_D_1
-#define nDs1 MI_Ds_1
-#define nE1 MI_E_1
-#define nF1 MI_F_1
-#define nFs1 MI_Fs_1
-#define nG1 MI_G_1
-#define nGs1 MI_Gs_1
-#define nA1 MI_A_1
-#define nAs1 MI_As_1
-#define nB1 MI_B_1
-
-#define nC1 MI_C_1
-#define nCs1 MI_Cs_1
-#define nD1 MI_D_1
-#define nDs1 MI_Ds_1
-#define nE1 MI_E_1
-#define nF1 MI_F_1
-#define nFs1 MI_Fs_1
-#define nG1 MI_G_1
-#define nGs1 MI_Gs_1
-#define nA1 MI_A_1
-#define nAs1 MI_As_1
-#define nB1 MI_B_1
-
-#define nC1 MI_C_1
-#define nCs1 MI_Cs_1
-#define nD1 MI_D_1
-#define nDs1 MI_Ds_1
-#define nE1 MI_E_1
-#define nF1 MI_F_1
-#define nFs1 MI_Fs_1
-#define nG1 MI_G_1
-#define nGs1 MI_Gs_1
-#define nA1 MI_A_1
-#define nAs1 MI_As_1
-#define nB1 MI_B_1
-
-#define nC1 MI_C_1
-#define nCs1 MI_Cs_1
-#define nD1 MI_D_1
-#define nDs1 MI_Ds_1
-#define nE1 MI_E_1
-#define nF1 MI_F_1
-#define nFs1 MI_Fs_1
-#define nG1 MI_G_1
-#define nGs1 MI_Gs_1
-#define nA1 MI_A_1
-#define nAs1 MI_As_1
-#define nB1 MI_B_1
-
-#define nC1 MI_C_1
-#define nCs1 MI_Cs_1
-#define nD1 MI_D_1
-#define nDs1 MI_Ds_1
-#define nE1 MI_E_1
-#define nF1 MI_F_1
-#define nFs1 MI_Fs_1
-#define nG1 MI_G_1
-#define nGs1 MI_Gs_1
-#define nA1 MI_A_1
-#define nAs1 MI_As_1
-#define nB1 MI_B_1
-
-
 #define ToOneHand TO(_OHLEFT)
 #define RightHand MO(_OHRIGHT)
 #define ToOHNumbers TO(_OHNUMBER)
@@ -229,46 +150,16 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ALPHA] = LAYOUT(
-    _,   _Q, _W, _F, _P, _B, _J, _L, _U, _Y, Tmux, _,
-    Alt, _A, _R, _S, _T, _G, _M, _N, _E, _I, _O, _,
-    _,   _Z, _X, _C, _D, _V, _K, _H, Alt, _, _, _,
-    ToOneHand, _,  _, Command, Numbers,  Space,  Mod, Control, _, Rgb, QK_BOOT
+    Alt,     _Q, _W, _F, _P, _B, _J, _L, _U, _Y, Tmux, _,
+    Command, _A, _R, _S, _T, _G, _M, _N, _E, _I, _O, Command,
+    Control, _Z, _X, _C, _D, _V, _K, _H, Alt, _, Alt, Control,
+    _, _,  _, _, Numbers,  Space,  Mod, Alt, _, _, Rgb
 ),
-[_OHLEFT] = LAYOUT(
-    ToOHNumbers, _Q, _W, _F, _P, _B, _, _, _, _, _, _,
-    Backspace, _A, _R, _S, _T, _G, _, _, _, _, _, _,
-    _, _Z, _X, _C, _D, _V, _, _, _, _, _, _,
-    ToAlpha, _,  _, _, RightHand,  Space,  _, _, _, _, _
-),
-[_OHRIGHT] = LAYOUT(
-    _, _,  _Y, _U, _L, _J, _, _, _, _, _, _,
-    _, _O, _I, _E, _N,_M, _, _, _, _, _, _,
-    _, _, OSControl, OSCommand, _H, _K, _, _, _, _, _, _,
-    _, _,  _, _, _,  Space,  _, _, _, _, _
-),
-[_OHNUMBER] = LAYOUT(
-    _, _,  _1, _2, _3, _,       _, _, _, _, _, _,
-    ToAlpha, _0, _4, _5, _6, _,       _, _, _, _, _, _,
-    _, _,  _7, _8, _9, _,       _, _, _, _, _, _,
-    _, _,  _,  _,  _,           _, _, _, _, _, _
-),
-// [_NOTES] = LAYOUT(
-//     nC,  nCs,  nD,  nDs,  nE,  nF,  nFs,  nG,  nGs,  nA,  nAs,  nB,
-//     nC1, nCs1, nD1, nDs1, nE1, nF1, nFs1, nG1, nGs1, nA1, nAs1, nB1,
-//     nC2, nCs2, nD2, nDs2, nE2, nF2, nFs2, nG2, nGs2, nA2, nAs2, nB2,
-//     _,   _,    _,   _,     _,   Notes2,   _,   _,    _,   _,    _
-// ),
-// [_NOTES2] = LAYOUT(
-//     nC3, nCs3, nD3, nDs3, nE3, nF3, nFs3, nG3, nGs3, nA3, nAs3, nB3,
-//     nC4, nCs4, nD4, nDs4, nE4, nF4, nFs4, nG4, nGs4, nA4, nAs4, nB4,
-//     nC5, nCs5, nD5, nDs5, nE5, nF5, nFs5, nG5, nGs5, nA5, nAs5, nB5,
-//     _, _, _, Velocity,App, _, _, Channel, _, _, _
-// ),
 [_MOD] = LAYOUT(
     _, Settings,  _,       Up,      Lang ,     _,       _, LightDec, LightInc, SoundDec, SoundInc, _,
     _, Alfred,    Left,    Down,    Right,     _,       _, Esc,      Tab,      _,        rcmd,     _,
     _, Smaller,   Bigger,  NewLine, Enter,     Buffer,  _, PgDn,     PgUp,     Home,     End,      _,
-    _, _,         _,       _,       Backspace, DelWord, _,           _,        _,        _,        _
+    _, _,         _,       _,       Backspace, DelWord, _,           _,        _,        _,        QK_BOOT
 ),
 [_NUMBER] = LAYOUT(
     _, _,  _1, _2, _3, _,       _,         Exlm,     Question,  Slash, _,      _,
@@ -282,12 +173,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _,_,     _,     KC_LBRC, KC_RBRC, _, _, Plus,     Minus,   Underscore, Equal,_,
     _,_, _, _, _, _, _, _, _, _, _
 ),
-[_OHRIGHT] = LAYOUT(
-    _, _,  _Y, _U, _L, _J, _, _, _, _, _, _,
-    _, _O, _I, _E, _N,_M, _, _, _, _, _, _,
-    _, _, OSControl, OSCommand, _H, _K, _, _, _, _, _, _,
-    _, _,  _, _, _,  Space,  _, _, _, _, _
-),
 [_RGB] = LAYOUT(
     RGB_TOG, RGB_MOD, RGB_RMOD, _, _, _, _, _, _, _, _, _,
     _, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _, _, _, _, _, _, _,
@@ -298,18 +183,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     _, _, WARPPTAB, WARPNTAB,_, _, _, _, ToAlpha, ToNotes,_,_,
 //     _, DM_REC1, DM_RSTP, DM_PLY1, _, _, _, _, _, _, _, _,
 //     QK_BOOT, PRNTSC1, PRNTSC2, PRNTSC3, _, _, _, _, _, _, _, _,
-//     _, _, _, _, _, _, _, _, _, _, _
-// ),
-// [_VELO] = LAYOUT(
-//     _, _, _, _, _, _, _, _, MI_VEL_1,  MI_VEL_2,  MI_VEL_3, _,
-//     _, _, _, _, _, _, _, MI_VELD, MI_VEL_4,  MI_VEL_5,  MI_VEL_6, MI_VEL_0,
-//     _, _, _, _, _, _, _, MI_VELU, MI_VEL_7,  MI_VEL_8,  MI_VEL_9, MI_VEL_10,
-//     _, _, _, _, _, _, _, _, _, _, _
-// ),
-// [_CHNL] = LAYOUT(
-//     _, MI_CH1,  MI_CH2,  MI_CH3,  _, _, _, _, MI_CH11, MI_CH12, MI_CH13, _,
-//     MI_CH10, MI_CH4,  MI_CH5,  MI_CH6,  _, _, _, _, MI_CH14, MI_CH15, MI_CH16, _,
-//     _, MI_CH7,  MI_CH8,  MI_CH9,  _, _, _, _, _, _, _, _,
 //     _, _, _, _, _, _, _, _, _, _, _
 // ),
 };
