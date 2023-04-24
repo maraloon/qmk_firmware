954b2ab58ff3b0836ccfb4743be52a1e97173d2b
mara <mara@the-witch.ru>
2023-04-25 01:48:41 +0400

    standart colemak

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/steno/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/steno/keymap.c
@@ -366,10 +366,10 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ALPHA] = LAYOUT(
-    _RT,  _W, _F, _P, _RF,_, _, _RU,_J, _U, _Y, Tmux,
-    _N, _R, _S, _T, _B, _, _, _RE,_A, _E, _I, _O,
+    _RT,  _W, _F, _P, _B,_, _, _J,_L,_U, _Y, Tmux,
+    _N, _R, _S, _T, _G, _, _, _M,_A, _E, _I, _O,
     Lang,  _X, _C, _D, _V, _, _, _K, _H, _Z, _Q, MO(_NAVIGATION),
-    _,  _,  _RH,_G, Space,   __, _L, _M, _RC,_,  _
+    _,  _,  _RH,_, Space,   __, _, _, _RC,_,  _
 ),
 [_RECTANGLE] = LAYOUT(
     _,  PrevDisplay, TopHalf, NextDisplay, _,  _, _, _,  _, _, _, _,
