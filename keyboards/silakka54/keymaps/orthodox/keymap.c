052e7cb91a8d14878e94e37057cc62ed02738c9c
mara <mara@the-witch.ru>
2026-07-04 00:43:29 +0300

    symbols on ABC

--- keyboards/silakka54/keymaps/orthodox/keymap.c
+++ keyboards/silakka54/keymaps/orthodox/keymap.c
@@ -176,11 +176,11 @@ enum my_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [ABC] = LAYOUT(
-    _, Unds,  Slash, Esc,  _,  _,                    _,     _,     Enter, Comma,    oC,   OSL(FN),
+    Hash, Percent,  Slash, Esc,  Unds,  Star,        _,     _,     Enter, Comma,    oC,   OSL(FN),
     Tab,   B,     L,     D,    W,    Dot,            Minus, F,     O,     U,     OSL(TMUX),   OSL(CODE),
- C(Bs),    N,     R,     T,    St,     G,            Y,     H,     A,     E,     I,   oS,
+ C(Bs),    N,     R,     T,    St,     G,            Y,     H,     A,     E,     I,   DDot,
     _,     Q,     X,     M,    Ct,     V,            K,     P,  OSL(SYM), SMART_NUM, Lets, End,
-                           Bs, Space, oS,            LANG, Z, J
+                           Bs, Space, oS,            LANG, LT(NUM, Z), J
   ),
 
   [RUS] = LAYOUT(
@@ -203,10 +203,10 @@ C(Bs), DUMB_NUM, _1, _2,      _3,  _,       _, _5, _6, _8, STRES, _,
   ),
 
   [SYM] = LAYOUT(
-    _, Star, Tag, tag, Esc, _,                   _, _, Enter, _,   _,  _,
+    _, _, Tag, tag, Esc, _,                   _, _, Enter, _,   _,  _,
 _, Borrow, borrow, Caret, Dollar, Percent,    Pipe, _, _, _, _,  OSL(CODE),
-BSlash, Equal, At, Bracket, bracket, Hash,    Amp, Array,  array,  Star,  STRES, _,
- Grave, Quest,  Exlm, DQuote, Quote, Plus,     Tilda, DDot,  DComm,  OSL(NUM), _,  _,
+BSlash, Equal, At, Bracket, bracket, _,       Amp, Array,  array,  Star,  STRES, _,
+ Grave, Quest,  Exlm, DQuote, Quote, Plus,    Tilda, _,  DComm,  OSL(NUM), _,  _,
                            _, QK_LLCK, _,     STRES, STRES, _
   ),
 
