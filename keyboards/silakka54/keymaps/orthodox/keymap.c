95d7687b70897a38c08c4de58790bbe13c81529b
mara <mara@the-witch.ru>
2026-07-04 21:42:09 +0300

    .

--- keyboards/silakka54/keymaps/orthodox/keymap.c
+++ keyboards/silakka54/keymaps/orthodox/keymap.c
@@ -223,10 +223,10 @@ BSlash, Equal, At, Bracket, bracket, Pipe,    Amp, Array,  array,  _,  STRES, _,
 
   [TMUX] = LAYOUT(
     _,     LALT(Left),     LALT(Up),     LALT(Down),     LALT(Right),     _,            _,     LSA(Left),     LSA(Up),     LSA(Down),     LSA(Right),   _,
-    _, LALT(B), LALT(L), LALT(D), LALT(W),     _,            _, OSL(CODE), LALT(O), LALT(U), LALT(J),   _,
+    _, LALT(B), LALT(L), LALT(D), LALT(W),     _,           oCA, OSL(CODE), LALT(O), LALT(U), LALT(J),   _,
     OS_LSA, LALT(N), LALT(R), LALT(T), LALT(St), LALT(G),   LALT(Y), LALT(H), LALT(A), LALT(E), LALT(I),   OS_LSA,
-    _, LALT(Q), LALT(X), LALT(M), LALT(Ct), LALT(V),       LALT(K), oCA, LALT(P), _, _, _,
-                 LALT(Z), QK_LLCK, LALT(Space),            STRES, _, _
+    _, LALT(Q), LALT(X), LALT(M), LALT(Ct), LALT(V),        LALT(K), oCA, LALT(P), _, _, _,
+                 LALT(Z), QK_LLCK, LALT(Space),             STRES, _, _
   ),
 
   [FN] = LAYOUT(
