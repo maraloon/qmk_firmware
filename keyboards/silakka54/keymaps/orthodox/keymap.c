4bbde53caf223b92b6c143be2b7ce47ca680001d
mara <mara@the-witch.ru>
2026-07-13 04:01:45 +0300

    Revert "swap space and shift"
    This reverts commit 97fe1c1102f6c481969b0ea94047b5648f4f9934.

--- keyboards/silakka54/keymaps/orthodox/keymap.c
+++ keyboards/silakka54/keymaps/orthodox/keymap.c
@@ -180,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     Tab,   B,     L,     D,    W,    Dot,            Minus, F,     O,     U,     OSL(TMUX),   oC,
  C(Bs),    N,     R,     T,    St,     G,            Y,     H,     A,     E,     I,   DDot,
     Z,     Q,     X,     M,    Ct,     V,            K,     P,  OSL(SYM), SMART_NUM, Lets, End,
-                  Bs, oS, LT(NUM, Space),            LANG, LT(SYM, Esc), J
+                  Bs, LT(NUM, Space), oS,            LANG, LT(SYM, Esc), J
   ),
 
   [RUS] = LAYOUT(
