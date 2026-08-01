676acc6d483151f5a8bcc56a41f01532cef844b7
mara <mara@the-witch.ru>
2026-08-01 19:49:17 +0300

    Revert "WinArrange"
    This reverts commit 3c8d3a2b5fce07d3509e5bebb2a28d9c30ec0fa8.

--- keyboards/silakka54/keymaps/orthodox/keymap.c
+++ keyboards/silakka54/keymaps/orthodox/keymap.c
@@ -162,7 +162,6 @@ enum my_keycodes {
 #define Dollar KC_DLR
 
 #define Lets LGUI(KC_F)
-#define WinArrange LGUI(KC_W)
 
 #define rF KC_KP_1 // ф
 #define rJ KC_KP_2 // ж
@@ -178,11 +177,11 @@ enum my_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [ABC] = LAYOUT(
-    Hash, Percent,  Slash, Comma, Unds,  Star,       WinArrange, Exlm,  Enter, Quest,    J,   _,
+    Hash, Percent,  Slash, Comma, Unds,  Star,       _, Exlm,  Enter, Quest,    _,   _,
     Tab,   B,     L,     D,    W,    Dot,            Minus, F,     O,     U,     OSL(TMUX),   oC,
  C(Bs),    N,     R,     T,    St,     G,            Y,     H,     A,     E,     I,   DDot,
     Z,     Q,     X,     M,    Ct,     V,            K,     P,  OSL(SYM), SMART_NUM, Lets, End,
-                  Bs, LT(NUM, Space), oS,            LANG, LT(SYM, Esc), WinArrange
+                  Bs, LT(NUM, Space), oS,            LANG, LT(SYM, Esc), J
   ),
 
   [RUS] = LAYOUT(
